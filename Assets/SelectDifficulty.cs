29c447afc69e655e81e09ff1c257e4827ccb168e
Christian Fredericksen <christian.fredericksen@yahoo.com>
2022-03-15 18:30:03 -0700

    created drop down menu for level selection

--- Assets/SelectDifficulty.cs
+++ Assets/SelectDifficulty.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class SelectDifficulty : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public enum SelectLevel
     {
-        
+        Easy,
+        Normal,
+        Hard,
+        Expert
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public SelectLevel currentLevel;
 }
