7c121505fb5451f47ca1eacd580e6fc71a88f441
Christian Fredericksen <christian.fredericksen@yahoo.com>
2022-03-15 22:18:20 -0700

    created itemDB script to keep track of items

--- Assets/ItemDB.cs
+++ Assets/ItemDB.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class ItemDB : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<Item> itemDB = new List<Item>();
 }
