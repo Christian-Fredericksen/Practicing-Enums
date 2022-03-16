3b1e916614bc1e68935c08168117777cb474be7e
Christian Fredericksen <christian.fredericksen@yahoo.com>
2022-03-15 18:50:09 -0700

    created switch statement for level selector

--- Assets/SelectDifficulty.cs
+++ Assets/SelectDifficulty.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SelectDifficulty : MonoBehaviour
 {
-    public enum SelectLevel
+    public enum LevelSelector
     {
         Easy,
         Normal,
@@ -12,5 +12,24 @@ public class SelectDifficulty : MonoBehaviour
         Expert
     }
 
-    public SelectLevel currentLevel;
+    public LevelSelector currentLevel;
+
+     private void Start()
+    {
+        switch(currentLevel)
+        {
+            case LevelSelector.Easy:
+                Debug.Log("You have selected level Easy.");
+                break;
+            case LevelSelector.Normal:
+                Debug.Log("You have selected level Normal.");
+                break;
+            case LevelSelector.Hard:
+                Debug.Log("You have selected level Hard.");
+                break;
+            case LevelSelector.Expert:
+                Debug.Log("You have selected level Expert.");
+                break;
+        }
+    }
 }
